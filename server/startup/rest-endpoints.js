c02fba5a3efcf92f08a126b04856df340a0910bc
mariam <mariam.abdussalam@andela.com>
2017-03-05 23:37:41 +0100

    chore(endpoint): add createProduct access

--- server/startup/rest-endpoints.js
+++ server/startup/rest-endpoints.js
@@ -1,4 +1,4 @@
-import { Shops,  Accounts, Cart, Orders, Products } from '/lib/collections';
+import { Shops, Accounts, Cart, Orders, Products } from '/lib/collections';
 
 export default () => {
   const Api = new Restivus({
@@ -6,34 +6,82 @@ export default () => {
     prettyJson: true
   });
 
-  const additional = (collection) => {
+  const apiOptions = (collection) => {
     return {
       routeOptions: {
         authRequired: true
       },
       endpoints: {
         put: {
-          roleRequired: ['owner', 'admin'],
-          action: () => {
-            const isUpdated = collection.update(this.urlParams.id, {
-              $set: this.bodyParams
-            });
-            if (isUpdated) {
-              return { statusCode: 201, status: 'success', data: isUpdated }
+          action: function () {
+            if (Roles.userIsInRole(this.userId, ['owner', 'admin'])) {
+              const isUpdated = collection.update(this.urlParams.id, {
+                $set: this.bodyParams
+              });
+              if (isUpdated) {
+                return { statusCode: 201, status: 'success', data: isUpdated }
+              }
+              return { status: 'fail', message: 'Record not found' }
             }
-            return { status: 'fail', message: 'Record not found' }
+            return {
+              statusCode: 403,
+              message: ' You are not allowed to access this route '
+            };
           }
         },
-
         delete: {
           roleRequired: 'admin'
         }
       }
     };
   };
-  Api.addCollection(Shops, additional(Shops));
-  Api.addCollection(Accounts, additional(Accounts));
-  Api.addCollection(Cart, additional(Cart));
-  Api.addCollection(Orders, additional(Orders));
-  Api.addCollection(Products, additional(Products));
+  const productApi = (collection) => {
+    return {
+      routeOptions: {
+        authRequired: true
+      },
+      endpoints: {
+        post: {
+          action: function () {
+            if (Roles.userIsInRole(this.userId, ['createProduct'])) {
+              const isUpdated = collection.insert(this.bodyParams);
+              if (isUpdated) {
+                return { statusCode: 201, status: 'success', data: isUpdated }
+              }
+              return { status: 'fail', message: 'Record not found' }
+            }
+            return {
+              statusCode: 403,
+              message: ' You are not allowed to access this route'
+            };
+          }
+        },
+        put: {
+          action: function () {
+            if (Roles.userIsInRole(this.userId, ['admin'])) {
+              const isUpdated = Products.upsert(this.urlParams.id, {
+                $set: this.bodyParams
+              });
+              if (isUpdated) {
+                return { statusCode: 201, status: 'success', data: isUpdated };
+              }
+              return { status: 'fail', message: 'Product not found' }
+            }
+            return {
+              statusCode: 403,
+              message: ' You are not allowed to access this route'
+            };
+          }
+        },
+        delete: {
+          roleRequired: 'admin'
+        }
+      }
+    };
+  };
+  Api.addCollection(Shops, apiOptions(Shops));
+  Api.addCollection(Accounts, apiOptions(Accounts));
+  Api.addCollection(Orders, apiOptions(Orders));
+  Api.addCollection(Cart, apiOptions(Cart));
+  Api.addCollection(Products, productApi(Products));
 };
