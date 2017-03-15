15e0eeea4d02e243b5b3dd4b59aeeb2142939cb1
mariam <mariam.abdussalam@andela.com>
2017-03-15 11:36:06 +0100

    remove unused code

--- imports/plugins/included/inventory/server/methods/statusChanges.js
+++ imports/plugins/included/inventory/server/methods/statusChanges.js
@@ -74,18 +74,6 @@ Meteor.methods({
           Products.upsert({ _id }, { $set: productUpdate }, { selector: { type: "simple" } });
         }
       }
-//       +      if (reservationStatus === "sold") {
-//  +        const _id = item.productId;
-//  +        const product = Products.findOne(_id);
-//  +        // check that a product with the _id exists so we don't end up creating a new product with upsert
-//  +        if (product) {
-//  +          const newQuantitySold = product.quantitySold + item.quantity;
-//  +          const productUpdate = Object.assign({}, product, {quantitySold: newQuantitySold});
-//  +          // Might be a bug, but "update" refused to work, hence the use of the "upsert"" method.
-//  +          Products.upsert({_id}, {$set: productUpdate}, {selector: {type: "simple"}});
-//  +        }
-//  +      }
-//       // check of existing reserved inventory for this cart
 
       const existingReservations = Inventory.find({
         productId: item.productId,
