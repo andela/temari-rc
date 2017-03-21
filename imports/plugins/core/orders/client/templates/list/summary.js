19b09b437ad93232c5a1651c4033a93f990b3a42
Chiamaka V. Nwosu <chiamaka.nwosu@andela.com>
2017-03-21 11:48:10 +0100

    Feature/141649859/adjust inventory (#8)
    * feature(update-inventory): reduce product quantity when order is made
    
    * feature(adjust-inventry): reduce and increase inventry stock and redirect after cancel order

--- imports/plugins/core/orders/client/templates/list/summary.js
+++ imports/plugins/core/orders/client/templates/list/summary.js
@@ -1,3 +1,4 @@
+import { Reaction } from "/client/api";
 import { Template } from "meteor/templating";
 import { Meteor } from "meteor/meteor";
 import { NumericInput } from "/imports/plugins/core/ui/client/components";
@@ -37,15 +38,16 @@ Template.ordersListSummary.events({
     const order = Template.instance().orders.get();
     // Make a method call to the server-side to cancel order.
     Meteor.call("orders/cancelOrder", order, (err, result) => {
-      // result returns product if cancel order is succesful and 0 if false.
+      // result returns 1 if cancel order is succesful and 0 if false.
+      if (result === 1) {
+        swal("", "Order Canceled", "success").then(function () {
+          return Reaction.Router.go("/");
+        });
+      } else {
+        swal("", "Unable to Cancel", "error");
+      }
 
-      // @TODO: method updates products
-      // Meteor.call("orders/updateProduct", result, (error, res) => {
-      //   // Do nothing
-      // });
-      return (typeof result === "object")
-        ? swal("", "Order Canceled", "success")
-        : swal("", "Unable to Cancel", "error");
+      Meteor.call("orders/inventoryAdjust", order._id, "cancel");
     });
   }
 });
